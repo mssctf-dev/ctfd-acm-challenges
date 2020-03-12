06c3d0fa22441e7bf41ee147379bd86b9460b665
Frank <frankli0324@hotmail.com>
2020-03-13 01:42:09 +0800

    require auth

--- view.py
+++ view.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
 from CTFd.models import Solves, Challenges
-from CTFd.utils.decorators import admins_only
+from CTFd.utils.decorators import admins_only, authed_only
 from .models import DynICPCModel, PSubmission
 
 views = Blueprint('acm_chall_views', __name__,
@@ -9,6 +9,7 @@ views = Blueprint('acm_chall_views', __name__,
 
 
 @views.route('/board')
+@authed_only
 def scoreboard():  # list tasks (scoreboard)
     challenges = DynICPCModel.query.all()
 
@@ -16,23 +17,28 @@ def scoreboard():  # list tasks (scoreboard)
 
 
 @views.route('/judge_queue')
+@authed_only
 def queue():
     PSubmission.query.all()
     return render_template('judge_queue.html', admin=False)
 
 
 @views.route('/admin/board')
+@authed_only
 @admins_only
 def admin_scoreboard():  # tasks running
     return render_template('board.html', admin=True)
 
 
 @views.route('/admin/judge_queue')
+@authed_only
+@admins_only
 def admin_queue():
     return render_template('judge_queue.html', admin=True)
 
 
 @views.route('/admin/cases')
+@authed_only
 @admins_only
 def cases():  # manage judge cases
     return render_template('case.html')
